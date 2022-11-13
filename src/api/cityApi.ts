2aa41f55b707c41bc445253498c9f20c8a9d86d5
leeshaun <kinhkha95@gmail.com>
2022-11-13 16:25:22 +0700

    setup api for city resource

--- src/api/cityApi.ts
+++ src/api/cityApi.ts
@@ -1,10 +1,17 @@
+import { City, ListResponse } from 'models';
 import axiosClient from './axiosClient';
 
 const cityApi = {
-    getAll() {
+    getAll(): Promise<ListResponse<City>> {
         const urlPath = '/cities';
-        return axiosClient.get(urlPath);
-    }
+        return axiosClient.get(urlPath, 
+            {
+                params: {
+                    _page: 1,
+                    _limit: 10,
+                },
+            });
+    },
 }
 
 export default cityApi;
